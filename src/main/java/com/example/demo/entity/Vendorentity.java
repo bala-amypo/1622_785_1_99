e4db73449698996cfd7fbddb8de4e28a3dbf3b9d
bala-amypo <bala@amypo.in>
2025-12-18 17:31:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vendorentity.java
+++ src/main/java/com/example/demo/entity/Vendorentity.java
@@ -1,5 +1,10 @@
 package com.example.demo.entity;
 
 public class Vendorentity{
-    
+    private Long id;
+    private String vendorname;
+    private String contactemail;
+    private String phone;
+    private String createdat;
+public void 
 }
