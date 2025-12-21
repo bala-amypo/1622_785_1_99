43e85a403b7cc03e5e62ed473a57e963cd76a5c0
bala-amypo <bala@amypo.in>
2025-12-21 16:01:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vendorentity.java
+++ src/main/java/com/example/demo/entity/Vendorentity.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 @Entity
