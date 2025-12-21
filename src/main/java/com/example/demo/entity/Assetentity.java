31370ebfa0311d173501615d987c53541d94c671
bala-amypo <bala@amypo.in>
2025-12-21 10:40:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/Assetentity.java
+++ src/main/java/com/example/demo/entity/Assetentity.java
@@ -1,34 +1,21 @@
 package com.example.demo.entity;
+
 import jakarta.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Set;
+import lombok.*;
+
 @Entity
-@Table(name = "assets")
-public class Assetentity {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Asset {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true, nullable = false)
-    private String assetTag;
+
     private String assetName;
-    private LocalDate purchaseDate;
-    private Double purchaseCost;
-    private String status; // ACTIVE, MAINTENANCE, DISPOSED
-    private LocalDateTime createdAt;
 
-    @OneToOne(mappedBy = "asset")
-    private AssetDisposalentity disposal;
-
-    public Assetentity() {
-    }
-
-    public Assetentity(String assetTag, String assetName, LocalDate purchaseDate, Double purchaseCost) {
-        this.assetTag = assetTag;
-        this.assetName = assetName;
-        this.purchaseDate = purchaseDate;
-        this.purchaseCost = purchaseCost;
-        this.status = "ACTIVE";
-        this.createdAt = LocalDateTime.now();
-    }
+    private String status;
 }
+
