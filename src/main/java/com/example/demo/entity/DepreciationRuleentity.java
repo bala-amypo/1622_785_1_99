93241df1becf0b351d3242165a5dd9bba204ce4b
bala-amypo <bala@amypo.in>
2025-12-22 07:56:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/DepreciationRuleentity.java
+++ src/main/java/com/example/demo/entity/DepreciationRuleentity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class DepreciationRule {
+public class DepreciationRuleentity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
