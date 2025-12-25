89da014bb902b8de1c8ffd4fd3bd5fcbb6108688
bala-amypo <bala@amypo.in>
2025-12-25 16:17:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.example.demo.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Set;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "secretkey";
+
+    public String generateToken(String email, Long userId, Set<String> roles) {
+
+        return Jwts.builder()
+                .claim("email", email)
+                .claim("userId", userId)
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000)) // 1 day
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
